53a1c996e728f9680a68a3d8eede5b32742b71cd
ryangittings <ryan@madebykingfisher.co.uk>
2024-05-16 11:37:27 +0100

    Migrate from HTML to JS

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
-const comp         = require('@node-minify/core');
-const htmlMinifier = require('@node-minify/html-minifier');
-
+const compressor = require('node-minify');
 
 module.exports = {
-
   onPostBuild: async ({ inputs, constants, utils }) => {
 
     // Only continue in the selected deploy contexts
     if( !inputs.contexts.includes(process.env.CONTEXT) ) {
-      console.log('Not minifiying HTML in the context:', process.env.CONTEXT);
+      console.log('Not minifiying JS in the context:', process.env.CONTEXT);
       return;
     }
 
-    // Minify HTML
-    console.log('Minifiying HTML in the deploy context:', process.env.CONTEXT);
-    console.log('Minifiying HTML with these options:', inputs.minifierOptions || "Default");
+    // Minify JS
+    console.log('Minifiying JS in the deploy context:', process.env.CONTEXT);
+    console.log('Minifiying JS with these options:', inputs.minifierOptions || "Default");
 
     try {
-      await comp({
-        compressor: htmlMinifier,
-        input: constants.PUBLISH_DIR + '/**/*.html',
-        output: '$1.html',
-        replaceInPlace: true,
-        options: {
-          collapseWhitespace: false,
-          ...inputs.minifierOptions
-        }
+      await compressor({
+        compressor: 'uglifyjs',
+        input: constants.PUBLISH_DIR + '/**/*.js',
+        output: '$1.js',
+        replaceInPlace: true
       });
 
 
     } catch (error) {
-      utils.build.failPlugin('The Minify HTML plugin failed.', { error })
+      utils.build.failPlugin('The Minify JS plugin failed.', { error })
     }
 
   }
